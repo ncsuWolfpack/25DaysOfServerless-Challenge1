75f9b107733447cedf5965214ee37d2a15839f69
Erik Harris <erik.harris@remasonco.com>
2019-12-01 21:10:28 -0500

    initial commit

---
+++ Day1_Dreidel/Dreidel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_Dreidel
+{
+    public class Dreidel
+    {
+        private readonly Random _random = new Random();
+
+        public DreidelValue CurrentValue { get; private set; }
+
+        public async Task SpinAsync()
+        {
+            var newValue = _random.Next(1, 5);
+            CurrentValue = await DreidelValue.GetByIdAsync(newValue);
+        }
+    }
+}
---
+++ Day1_Dreidel/DreidelValue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_Dreidel
+{
+    public class DreidelValue
+    {
+        public int Id { get; set; }
+        public char Char { get; set; }
+
+        public string Name { get; set; }
+
+        public static DreidelValue Nun = new DreidelValue() { Id = 1, Char = '\u05E0', Name = "Nun" };
+        public static DreidelValue Gimmel = new DreidelValue() { Id = 2, Char = '\u05D2', Name = "Gimmel" };
+        public static DreidelValue Hay = new DreidelValue() { Id = 3, Char = '\u05D4', Name = "Hay" };
+        public static DreidelValue Shin = new DreidelValue() { Id = 4, Char = '\u05E9', Name = "Shin" };
+
+        public async static Task<DreidelValue> GetByIdAsync(int id)
+        {
+            var possibleValues = new List<DreidelValue>() { Nun, Gimmel, Hay, Shin };
+            var value = possibleValues
+                .Where(x => x.Id == id)
+                .FirstOrDefault();
+
+            return await Task.FromResult(value);
+        }
+
+        public override string ToString()
+        {
+            return $"{Char} : ({Name})";
+        }
+    }
+}
